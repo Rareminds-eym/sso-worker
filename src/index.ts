6f64307fe065468938c36712662edfdc2c926946
gokulrajrz <rajgokul9847161393@gmail.com>
2026-05-28 11:24:32 +0530

    feat: add observability sampling rates and X-Request-ID to 500 error responses

--- src/index.ts
+++ src/index.ts
@@ -259,7 +259,9 @@ export default class SsoWorker extends WorkerEntrypoint<Env> {
           ms: Date.now() - start,
         }),
       );
-      return withCors(error("Internal server error", 500), cors);
+      const errResponse = withCors(error("Internal server error", 500), cors);
+      errResponse.headers.set("X-Request-ID", requestId);
+      return errResponse;
     }
   }
 
